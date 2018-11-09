a7529c5f57bf0aa58fee7d84206090e49e80a519
wsampaio <well7cordas@yahoo.com.br>
2018-11-09 11:36:43 -0200

    Lista de exercícios 1 - env. em 04/10/18

---
+++ medias.py
@@ -0,0 +1,19 @@
+
+nota1 = input("Digite a primeira nota: ")
+print()
+
+nota2 = input("Digite a segunda nota: ")
+print()
+
+nota3 = input("Digite a terceira nota: ")
+print()
+
+nota4 = input("Digite a quarta nota: ")
+print()
+
+media = (int(nota1) + int(nota2) + int(nota3) + int(nota4)) / 4
+
+#dá pra melhorar
+#fica confuso os conversores dentro do calculo
+
+print("A média aritmética é", media)
---
+++ quadrado.py
@@ -0,0 +1,13 @@
+#Perímetro e área
+
+valor = input("Digite o valor correspondente ao lado de um quadrado: ")
+#input SEMPRE devolve string
+
+perimetro = int(valor) * 4
+area = int(valor) * int(valor)
+
+print("perímetro:", perimetro,  "- área:", area)
+#quando concatena com vírgulas
+#ele coloca espaços automaticos
+
+
