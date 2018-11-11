f78c2b36b428d582ff92d24e48aa38fc4eaa4810
WELL Sampaio <wsampaio@yahoo.com>
2018-11-11 12:23:30 -0200

    Exercícios adicionais - Semana 2

--- dezenas.py
+++ dezenas.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Digito das dezenas
 
 n = int(input("Digite um número inteiro: "))
@@ -7,11 +6,5 @@ digDezena = n // 10
 digDezena = digDezena % 10
 
 print("O dígito das dezenas é", digDezena)
-=======
-
-
-
-
->>>>>>> ac615cb0a09a8d1e05217c7a77a2fb5d4eceac5a
 
 
--- fatura.py
+++ fatura.py
@@ -8,4 +8,3 @@ valor = input("Digite o valor da fatura: ")
 print("Olá,", nome)
 print("A sua fatura com vencimento em", diaVenc, "de", mesVenc, "no valor de R$", valor, "está fechada.")
 
-
