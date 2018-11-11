12bb969d18d4b88312d0094aa011827dd02dce5f
WELL Sampaio <wsampaio@yahoo.com>
2018-11-11 13:49:58 -0200

    Exercícios adicionais - Semana 5 (correcao)

--- fizzbuzz_funcao.py
+++ fizzbuzz_funcao.py
@@ -1,16 +1,21 @@
 #FizzBuzz
 
 def fizzbuzz(n):
-    if (n % 3 == 0) and (n % 5 != 0):
-        return "Fizz"
-    elif (n % 3 != 0) and (n % 5 == 0):
-        return "Buzz"
-    elif (n % 3 == 0) and (n % 5 == 0):
-        return "FizzBuzz"
+    if n % 3 == 0:
+        if n % 5 == 0:
+            return "FizzBuzz"
+        else:
+            return "Fizz"
     else:
-        return n
+        if n % 5 == 0:
+            return "Buzz"
+        else:
+            return n
 
 #print(fizzbuzz(3)) #deve devolver Fizz
+#print(fizzbuzz(6)) #deve devolver Fizz
+#print(fizzbuzz(12)) #deve devolver Fizz
+#print(fizzbuzz(93)) #deve devolver Fizz
 #print(fizzbuzz(5)) #deve devolver Buzz
 #print(fizzbuzz(15)) #deve devolver FizzBuzz
 #print(fizzbuzz(4)) #deve devolver 4
