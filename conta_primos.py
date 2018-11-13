a74d857d4012085a9ad059bb3753c0c875886b96
WELL Sampaio <wsampaio@yahoo.com>
2018-11-13 12:56:04 -0200

    Exercícios adicionais - Semana 7

--- conta_primos.py
+++ conta_primos.py
@@ -1,6 +1,23 @@
 #Numero de primos
 
 def n_primos(n):
-    pass
+    soma = 0
+    while n > 1:
+        if ePrimo(n):
+            soma += 1
+        n -= 1
+    return soma
 
+def ePrimo(num):
+    verifica = 2
+    primo = True
+    while (verifica <= num) and primo:
+        if (num % verifica) == 0:
+            if num != verifica:
+                primo = False
+        verifica = verifica + 1
+    return primo
+
+#print(n_primos(5))
+#print(n_primos(11))
 
--- soma_hipotenusas.py
+++ soma_hipotenusas.py
@@ -1,6 +1,39 @@
 #Soma hipotenusas
 
+def e_hipotenusa(n):
+    verif = False
+    c1 = n
+    c2 = n
+
+    while c1 > (n/2):
+        while c2 > 0 and verif == False:
+            if (c1 ** 2) + (c2 ** 2) == n ** 2:
+                print(c1, "\t", c2, "\t", n, "\t", verif)
+                verif = True
+            c2 -= 1
+        #print(c1, "\t", c2, "\t", n, "\t", verif)
+        c1 -= 1
+        c2 = c1
+
+    if n in [5, 10, 13, 15, 17, 20, 25]:
+        #verif = True
+        pass
+
+    return verif
+
+
+
+
 def soma_hipotenusas(n):
-    pass
+    soma = 0
+    while n > 0:
+        if e_hipotenusa(n):
+            soma += n
+        n -= 1
+    return soma
+
+#print(soma_hipotenusas(20)) #retorna 105
+#print(soma_hipotenusas(25)) #retorna 105
+#print(soma_hipotenusas(200)) #retorna 10352
 
 
